1055239f01aee45b6431b7ef7a5234d356b3fa06
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2022-08-27 17:38:14 -0300

    feat: :sparkles: turn app weather parallel

--- lib/weather.ex
+++ lib/weather.ex
@@ -1,4 +1,14 @@
 defmodule App.Weather do
+  def start(cities) do
+    manager_pid = spawn(__MODULE__, :manager, [[], Enum.count(cities)])
+
+    cities
+    |> Enum.map(fn city ->
+      pid = spawn(__MODULE__, :get_temperature, [])
+      send(pid, {manager_pid, city})
+    end)
+  end
+
   def get_appid() do
     "8819eba4547de65464036a43261bac28"
   end
@@ -21,6 +31,31 @@ defmodule App.Weather do
 
       :error ->
         "#{location} not found"
+    end
+  end
+
+  def get_temperature() do
+    receive do
+      {manager_pid, location} ->
+        send(manager_pid, {:ok, temperature_of(location)})
+
+      _ ->
+        IO.puts("Error")
+    end
+
+    get_temperature()
+  end
+
+  def manager(cities \\ [], total) do
+    receive do
+      {:ok, temp} ->
+        results = [temp | cities]
+
+        if(Enum.count(results) == total) do
+          send(self(), :exit)
+        end
+
+        manager(cities, total)
     end
   end
 
