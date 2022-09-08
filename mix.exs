daae85b142d2dd140c13e5bf50775c10de891891
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2022-09-08 16:17:33 -0300

    feat: :sparkles: Config lint

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule App.MixProject do
     [
       {:httpoison, "~> 1.0"},
       {:json, "~> 1.4.0"},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 end
